a6e4ea2d6f4267a722bbfa11acd0a841d467e1ee
Pratik Prajapati <prajapatipratik798@gmail.com>
2024-01-01 14:23:43 +0545

    Added home page

--- index.js
+++ index.js
@@ -2,6 +2,24 @@ import express from "express";
 
 const app = express();
 
+app.use("/", (req, res) => {
+  res.status(200).send(`
+  <html>
+  <head>
+    <style>
+      h1 {
+        color: gray;
+      }
+    </style>
+  </head>
+    <body>
+      <h1>Hello my name is pratik</h1>
+      <p>This is my website</p>
+    </body>
+  </html>
+  `);
+});
+
 app.use("/blue", (req, res) => {
   return res.status(200).send("Hello, It's me.");
 });
